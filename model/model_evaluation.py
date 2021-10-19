9b14d70ac6e2222dbce9bfd39ce9b80c5b3666f2
Sreenath Srikrishnan <sreenath.srikrishnan@elucidata.io>
2021-10-19 00:45:09 -0400

    "Metatest with HNSC"

--- model/model_evaluation.py
+++ model/model_evaluation.py
@@ -12,6 +12,22 @@ from operator import add
 import time
 import argparse
 import json
+from numpy import vstack
+from pandas import read_csv
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch import Tensor
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import Sigmoid
+from torch.nn import Module
+from torch.optim import SGD
+from torch.nn import BCELoss
+from torch.nn.init import kaiming_uniform_
+from torch.nn.init import xavier_uniform_
 
 class DAPLModel(nn.Module):
 
@@ -36,4 +52,29 @@ model.load_state_dict(torch.load("800_iterations_pancan_v1.pt"))
 # model = torch.load("800_iterations_pancan_v1.pt")
 print(model.eval())
 
+# evaluate the model
+def evaluate_model(test_dl, model):
+    predictions, actuals = list(), list()
+    for i, (inputs, targets) in enumerate(test_dl):
+        # evaluate the model on the test set
+        yhat = model(inputs)
+        # retrieve numpy array
+        yhat = yhat.detach().numpy()
+        actual = targets.numpy()
+        actual = actual.reshape((len(actual), 1))
+        # round to class values
+        yhat = yhat.round()
+        # store
+        predictions.append(yhat)
+        actuals.append(actual)
+    predictions, actuals = vstack(predictions), vstack(actuals)
+    # calculate accuracy
+    acc = accuracy_score(actuals, predictions)
+    return acc
 
+
+# metatrain_test_data_path= ""
+
+
+sample_pancan = pd.read_csv("sample_data/pretrainPanCan/feature_train.csv")
+sample_pancan.shape
