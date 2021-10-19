587042659070e51dced5e547218a7499b94186e8
Sreenath Srikrishnan <sreenath.srikrishnan@elucidata.io>
2021-10-19 19:56:05 -0400

    "Porting torch from CPU to GPU"

--- fewshot_metatrain.py
+++ fewshot_metatrain.py
@@ -42,11 +42,11 @@ def do_base_learning(model, x_batch, R_matrix_batch, ystatus_batch, lr_inner, n_
     
     for i in range(n_inner):
         x_batch = torch.FloatTensor(x_batch)
-        x_batch = x_batch.to(device)
+        x_batch = x_batch.to(device, non_blocking=True)
         x_batch=Variable(x_batch,requires_grad=True )
 
         R_matrix_batch = torch.FloatTensor(R_matrix_batch)
-        R_matrix_batch = R_matrix_batch.to(device, non_blocking=True))
+        R_matrix_batch = R_matrix_batch.to(device)
         R_matrix_batch=Variable(R_matrix_batch,requires_grad = True )
 
         ystatus_batch = torch.FloatTensor(ystatus_batch)
